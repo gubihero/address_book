e49166d380e4fa594ad825420073f27421b09250
James Gubler <gubimail@gmail.com>
2015-06-18 13:57:23 -0600

    adds contacts and starts cleaning up layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
+  resources :contacts
   devise_for :users
-  root "application#welcome"
+  root "contacts#index"
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618184904) do
+ActiveRecord::Schema.define(version: 20150618192352) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.text     "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "contacts", ["user_id"], name: "index_contacts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
